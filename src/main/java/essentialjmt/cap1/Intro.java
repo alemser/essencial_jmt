01ebad5301a058f7f3d927d81567f7aa19e42e22
Alessandro L:emser <lemser.alessandro@gmail.com>
2017-08-04 20:04:03 +0000

    Intro adjustment

--- src/main/java/essentialjmt/cap1/Intro.java
+++ src/main/java/essentialjmt/cap1/Intro.java
@@ -3,7 +3,8 @@ package essentialjmt.cap1;
 public class Intro {
 
     public static void main(String[] args) {
-        
+        Long id = Thread.currentThread().getId();
+        System.out.println("Thread id " + id);
     }
     
 }
